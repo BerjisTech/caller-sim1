a43bab1e6afd48c4f2ad96423dd39f4f732f16eb
Benedict <berjistech.gmail.com>
2024-09-21 16:43:24 +0300

    refactor: Update room creator form UI and functionality
    - Update room creator form UI and functionality to improve user experience and ensure proper functionality.
    
    feat: Add Tailwind CSS configuration
    
    - Add Tailwind CSS configuration to the project for easier styling and consistent design.
    
    Update angular.json to disable CLI analytics
    
    - Update angular.json file to disable CLI analytics for improved performance and privacy.
    
    Update room component UI and functionality
    
    - Update room component UI and functionality to enhance the user interface and add new features.

--- src/app/pages/room/room.component.ts
+++ src/app/pages/room/room.component.ts
@@ -1,22 +1,51 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { DarkModeComponent } from "../../components/shared/dark-mode/dark-mode.component";
 
 @Component({
   selector: 'app-room',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, DarkModeComponent],
   templateUrl: './room.component.html',
   styleUrl: './room.component.scss',
 })
-export class RoomComponent {
+export class RoomComponent implements OnInit {
   public room_id: string = '';
+  public streams: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-  constructor(private activatedRoute: ActivatedRoute) {
-    this.activatedRoute.params.subscribe({
-      next: (params) => {
-        // Get :room_id
-        this.room_id = params['room_id'];
-      },
-    });
+  public video_sizes: {
+    width: number; // grid-template-columns count
+    height: number; // grid-template-rows count
+  } = {
+    width: 1,
+    height: 1,
+  };
+
+  ngOnInit(): void {
+    this.updateVideoSizes();
+  }
+
+  updateVideoSizes(): void {
+    this.video_sizes = this.calculateVideoSizes();
+  }
+
+  calculateVideoSizes(): { width: number; height: number } {
+    const num_streams = this.streams.length;
+
+    const width = Math.ceil(Math.sqrt(num_streams)); // Dynamic column count
+    const height = Math.ceil(num_streams / width);  // Dynamic row count
+
+    return { width, height };
+  }
+
+  // Generate grid styles dynamically
+  getGridStyles(): any {
+    const columns = `repeat(${this.video_sizes.width}, 1fr)`;
+    const rows = `repeat(${this.video_sizes.height}, 1fr)`;
+    return {
+      'grid-template-columns': columns,
+      'grid-template-rows': rows,
+    };
   }
 }
