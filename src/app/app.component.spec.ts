07275fc3ddc0d0d15db32ea241640f9c8f1864e6
Benedict <berjistech.gmail.com>
2024-10-05 22:06:04 +0300

    Refactor app component title and index.html metadata

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'caller-sim1' title`, () => {
+  it(`should have the 'Nichapie' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('caller-sim1');
+    expect(app.title).toEqual('Nichapie');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, caller-sim1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Nichapie');
   });
 });
