2cf3d245c7aa511bb44ee5b6aad35b3862c8503f
Benedict <berjistech.gmail.com>
2024-12-16 14:15:33 +0300

    Enhance streaming component: Update message name display to use consistent color classes and improve color selection logic

--- src/app/services/content/content.service.ts
+++ src/app/services/content/content.service.ts
@@ -43,13 +43,13 @@ export class ContentService {
 
   getRandomTailwindColorClass() {
     const colors = [
-      'bg-red-500',
-      'bg-yellow-500',
-      'bg-green-500',
-      'bg-blue-500',
-      'bg-indigo-500',
-      'bg-purple-500',
-      'bg-pink-500',
+      'red-500',
+      'yellow-500',
+      'green-500',
+      'blue-500',
+      'indigo-500',
+      'purple-500',
+      'pink-500',
     ];
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
