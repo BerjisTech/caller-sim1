91cd43dc11ff2828fec605ccdae6d2b12b77d0de
Benedict <berjistech.gmail.com>
2025-03-16 17:59:36 +0300

    Login login response

--- src/app/services/user/auth.service.ts
+++ src/app/services/user/auth.service.ts
@@ -29,6 +29,7 @@ export class AuthService {
         localStorage.setItem('currentUser', JSON.stringify(response.data));
         localStorage.setItem('r3_token', response.token);
         this.currentUserSubject.next(response.data);
+        console.log(response);
         return response;
       }));
   }
